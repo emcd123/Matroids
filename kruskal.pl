c0b86de8f402aa5da3d71341cf4fc8f38c292ab5
emcd123 <mcdonnell.owen1@gmail.com>
2018-03-12 14:11:59 +0000

    Almost ready to count edges

--- kruskal.pl
+++ kruskal.pl
@@ -3,6 +3,7 @@ use warnings;
 
 use Data::Dumper qw(Dumper);
 use Tie::IxHash;
+use List::MoreUtils qw(uniq);
 
 my $MAX_WEIGHT = 10;
 #Implementing Kruskal's algorithm
@@ -35,7 +36,6 @@ foreach my $k (@E){
 print Dumper(\%TEMP);
 
 my $vertex_count = @V;
-
 my $temp_edge = 'OO';
 while ($vertex_count > 1) {
 	sub MinWeightEdge{
@@ -45,7 +45,6 @@ while ($vertex_count > 1) {
 			if ($TEMP{$var} < $min_weight_edge) {
 				$min_weight_edge = $TEMP{$var};
 				$min_key = $var;		
-				#print $min_key;
 			}
 		}
 		delete $TEMP{$min_key};
@@ -65,7 +64,6 @@ while ($vertex_count > 1) {
 	MatchVertexEdge($temp_edge, %FOREST);
 }
 print Dumper(\%FOREST);
-#print Dumper(\%TEMP);
 acyclic(%FOREST);
 
 sub acyclic {
@@ -76,6 +74,11 @@ sub acyclic {
 		for ( my $m = 0; $m < $length; $m++) {
 			push @edge_set, $FOREST{$vertex}[$m];
 		}
+	}
+	my @uniq_edgeset = uniq(@edge_set);
+	
+	foreach my $edge (@uniq_edgeset){
+		print $edge;
 	}
 }
 exit;
