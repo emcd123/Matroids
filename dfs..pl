644ac6abbd3127adc6cda58f1f768f0b0d0bd0f9
emcd123 <mcdonnell.owen1@gmail.com>
2018-03-16 15:00:38 +0000

    Some cleanup on the repo

--- dfs..pl
+++
@@ -1,2 +0,0 @@
-my @edge_set = ('AB', 'BC','AC');
-my @vertices = ('A', 'B', 'C', 'D');
--- dfs.py
+++
@@ -1,8 +0,0 @@
-edge_set = ['AB', 'BC','AC'];
-vertices = ['A', 'B', 'C', 'D'];
-
-Discovered = []
-start_vertex = 'A'
-
-for i in edge_set:
-	
